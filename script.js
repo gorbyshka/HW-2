d4a026c48a56c0270efef6b1d4edf3931afdae53
GoRbYsKkA <gleb.kovalchuk0609@gmail.com>
2022-11-26 15:20:55 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const firstNumber = prompt('Введіть перше число');
+const secondNumber = prompt('Введіть друге число');
+
+const firstConvertion = Number(firstNumber);
+const secondConvertion = Number(secondNumber);
+
+const addResult = firstConvertion + secondConvertion;
+const subResult = firstConvertion - secondConvertion;
+const divResult = firstConvertion / secondConvertion;
+const multResult = firstConvertion * secondConvertion;
+
+alert('Користувач ввів ' + ': ' + firstNumber + '+' + secondNumber + '=' + addResult);
+alert('Користувач ввів ' + ': ' + firstNumber + '-' + secondNumber + '=' + subResult);
+alert('Користувач ввів ' + ': ' + firstNumber + '/' + secondNumber + '=' + divResult);
+alert('Користувач ввів ' + ': ' + firstNumber + '*' + secondNumber + '=' + multResult);
